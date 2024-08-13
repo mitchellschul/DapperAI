3d55bd2a971ef822520385b02f6c7d24e1c049b3
Mitch <70587519+mitchellschul@users.noreply.github.com>
2024-08-12 22:37:38 -0700

    updated closet

--- DapperAi/app/(tabs)/closet.tsx
+++ DapperAi/app/(tabs)/closet.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, Image, ScrollView, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
 import ItemCard from '@/components/ItemCard'
-import { getDatabase, ref, onValue } from "firebase/database";
 
 const WardrobeScreen: React.FC = () => {
   const items = [
@@ -20,14 +19,6 @@ const WardrobeScreen: React.FC = () => {
 
   const itemTypes = [...new Set(items.map(item => item.type))];
   const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
-
-  const database = getDatabase();
-  const itemsRef = ref(database, 'images/');
-  onValue(itemsRef, (snapshot) => {
-    const data = snapshot.val();
-    console.log(data);
-    // updateStarCount(postElement, data);
-  });
 
 
 
@@ -52,7 +43,7 @@ const WardrobeScreen: React.FC = () => {
       {/* Wardrobe items */}
       <ScrollView contentContainerStyle={styles.grid}>
         <View>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <ScrollView horizontal={true}>
             {itemTypes.map((type, index) => (
               <TouchableOpacity
                 key={index}
